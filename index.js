41526ec3706dbcefd0809ad3c372e4ec68818b88
vanessa082 <wahvanessa22@gmail.com>
2024-03-03 20:49:24 +0100

    array of handshake

---
+++ index.js
@@ -0,0 +1,7 @@
+const handshake = [
+  {1: 'wink'},
+  {10: 'double blink'},
+  {100: 'close your eyes'},
+  {1000: 'jump'},
+  {10000: 'Reverse the order of the operations in the secret handshake'}
+]
